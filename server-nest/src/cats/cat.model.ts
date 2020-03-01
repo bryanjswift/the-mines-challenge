2b6ea727936dda80d25e57a869928cb190cc28a2
Bryan Swift <mail@bryanwrit.es>
2020-02-29 20:41:30 -0500

    Add documentation to the cat model

--- server-nest/src/cats/cat.model.ts
+++ server-nest/src/cats/cat.model.ts
@@ -1,6 +1,13 @@
+/**
+ * Domain model reprsentation of a cat.
+ */
 export interface Cat {
+  /** Unique identifier for this cat. */
   id: string;
+  /** Name for this cat. */
   name: string;
+  /** ISO8601 date time at which this cat was born. */
   dateOfBirth: string;
+  /** Breed of this cat. */
   breed: string;
 }
