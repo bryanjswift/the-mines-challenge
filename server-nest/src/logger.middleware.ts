d0513ce3a4af867316915cbc4cdddf2c4b37b031
Bryan Swift <mail@bryanwrit.es>
2020-02-29 20:43:24 -0500

    Change the logger middleware to report timing

--- server-nest/src/logger.middleware.ts
+++ server-nest/src/logger.middleware.ts
@@ -1,11 +1,21 @@
 import { Request, Response, NextFunction } from 'express';
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
 
+/**
+ * Measure and log the time spent to generate a response.
+ */
 @Injectable()
-export class LoggerMiddleware {
+export class LoggerMiddleware implements NestMiddleware {
+  /**
+   * Time the execution of `next` in order to log it.
+   * @param req incoming request.
+   * @param res outgoing response.
+   * @param next function called to pass control to the next middleware
+   * function.
+   */
   use(req: Request, res: Response, next: NextFunction) {
-    Logger.debug('Start', req.path);
+    const start = Date.now();
     next();
-    Logger.debug('Stop', req.path);
+    Logger.debug(`Completed in ${Date.now() - start}ms`, req.path);
   }
 }
