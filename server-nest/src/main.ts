cc5dda88d4acd42193fd39e6acf34bcbce036fe7
Bryan Swift <mail@bryanwrit.es>
2020-03-02 21:11:38 -0500

    Log port on startup

--- server-nest/src/main.ts
+++ server-nest/src/main.ts
@@ -1,4 +1,5 @@
 import * as dotenv from 'dotenv';
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
@@ -7,5 +8,6 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT);
+  Logger.debug(`Started on port :${process.env.PORT}`);
 }
 bootstrap();
