4224ceb604b26220941bd7707306d1c2b3cb4c46
Bryan Swift <mail@bryanwrit.es>
2020-03-04 16:53:03 -0500

    Create the `Game` structure
    The `Game` generates the list of `Cell` instances. The list of `moves`
    was intended to reflect that the state of the game can be derived from a
    list of recorded moves based on the state of cells.
    
    The mutations on `Cell` were a concession to time because this
    information is calculable from the `Cell`s and moves as the list of
    `Cell` is not intended to be modified once it is created.

---
+++ server-nest/src/game/game.model.spec.ts
@@ -0,0 +1,15 @@
+import { Game } from './game.model';
+
+describe.only(Game, () => {
+  describe('2x2', () => {
+    let game: Game;
+
+    beforeEach(() => {
+      game = new Game({ rows: 2, columns: 2 });
+    });
+
+    it('has a game with cells', () => {
+      expect(game.cells).toHaveLength(4);
+    });
+  });
+});
---
+++ server-nest/src/game/game.model.ts
@@ -0,0 +1,59 @@
+import { v4 as uuid } from 'uuid';
+import { Cell, CellId } from './cell.model';
+
+enum GameStatus {
+  OPEN,
+  WIN,
+  LOST,
+}
+
+interface Props {
+  columns: number;
+  rows: number;
+  cells?: Cell[];
+}
+
+function generateCells(cellCount: number): Cell[] {
+  const cells = new Array(cellCount);
+  for (let i = 0; i < cellCount; i++) {
+    const isMine = Math.random() > 0.5;
+    this.cells[i] = new Cell({ isMine });
+  }
+  return cells;
+}
+
+export class Game {
+  cells: Cell[];
+  id: string;
+  moves: CellId[];
+  gameStatus: GameStatus;
+
+  constructor(props: Props) {
+    const { rows, columns } = props;
+    const cellCount = rows * columns;
+    if (props.cells === undefined) {
+      this.cells = generateCells(cellCount);
+    } else {
+      this.cells = props.cells;
+    }
+    this.id = uuid();
+    this.moves = [];
+  }
+
+  open(cellId: CellId): Game {
+    this.moves = [
+      cellId,
+      ...this.moves,
+    ];
+    const openedCell = this.cells.find(cell => cell.id === cellId);
+    if (openedCell === undefined) {
+      throw new Error(`Cell with id ${cellId} does not exist`);
+    }
+    openedCell.isOpen = true;
+    if (openedCell.isMine) {
+      this.gameStatus = GameStatus.LOST;
+    }
+    // if all cells are mine or open game is won
+    return this;
+  }
+}
