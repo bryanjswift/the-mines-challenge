abdaa1b360bae1a0e06d390e62003cf832c5e89d
Bryan Swift <mail@bryanwrit.es>
2020-03-03 22:20:00 -0500

    Leave the "parameter" names for readability

--- server-nest/src/cats/cats.resolver.ts
+++ server-nest/src/cats/cats.resolver.ts
@@ -2,15 +2,18 @@ import { Args, Query, Resolver } from '@nestjs/graphql';
 import { Cat } from './cat.model';
 import { CatsService } from './cats.service';
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 @Resolver(of => Cat)
 export class CatsResolver {
   constructor(private readonly catsService: CatsService) {}
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   @Query(returns => [Cat])
   async list(): Promise<Cat[]> {
     return this.catsService.list();
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   @Query(returns => Cat, { nullable: true })
   getCat(@Args('id') id: string) {
     return this.catsService.findById(id);
