670d628f4c9a5e12233a7e5cfe3aed19390a307e
Bryan Swift <mail@bryanwrit.es>
2020-03-03 22:24:17 -0500

    Add a default value for `PORT` in server-nest

--- server-nest/src/main.ts
+++ server-nest/src/main.ts
@@ -4,10 +4,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 dotenv.config();
+const port = process.env.PORT || 3000;
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT);
-  Logger.debug(`Started on port :${process.env.PORT}`);
+  await app.listen(port);
+  Logger.debug(`Started on port :${port}`);
 }
 bootstrap();
