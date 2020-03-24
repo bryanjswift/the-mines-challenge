a3b24a914f3cff1481a08d1411ff37d0d4333272
Bryan Swift <mail@bryanwrit.es>
2020-03-24 04:37:47 -0400

    Add specs to cover `CatsService`

--- server-nest/src/cats/cats.service.spec.ts
+++ server-nest/src/cats/cats.service.spec.ts
@@ -1,10 +1,12 @@
 import { date, name, random } from 'faker';
+import { ulid } from 'ulid';
+import { Cat } from './cat.model';
 import { CatsService } from './cats.service';
 
 describe(CatsService, () => {
   let service: CatsService;
 
-  beforeAll(() => {
+  beforeEach(() => {
     service = new CatsService();
   });
 
@@ -49,6 +51,46 @@ describe(CatsService, () => {
       expect(result.name).toEqual(input.name);
       expect(result.breed).toEqual(input.breed);
       expect(result.dateOfBirth).toEqual(input.dateOfBirth);
+    });
+  });
+
+  describe('#list', () => {
+    it('is empty', () => {
+      const result = service.list();
+      expect(result).toHaveLength(0);
+    });
+
+    it('has contents after create', () => {
+      const cat = service.create({
+        name: name.firstName(),
+        breed: random.word(),
+        dateOfBirth: date.past().toISOString(),
+      });
+      const result = service.list();
+      expect(result).toHaveLength(1);
+      expect(result).toContain(cat);
+    });
+  });
+
+  describe('#findById', () => {
+    let cat: Cat;
+
+    beforeEach(() => {
+      cat = service.create({
+        name: name.firstName(),
+        breed: random.word(),
+        dateOfBirth: date.past().toISOString(),
+      });
+    });
+
+    it('is undefined for unknown id', () => {
+      const result = service.findById(ulid());
+      expect(result).toBeUndefined();
+    });
+
+    it('finds the an existing game', () => {
+      const result = service.findById(cat.id);
+      expect(result).toBe(cat);
     });
   });
 });
