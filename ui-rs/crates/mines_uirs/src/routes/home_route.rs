58fc6e1305dda4aab422d67f2a82152bd2e0fd77
Bryan Swift <bryan@bryanjswift.com>
2020-08-20 00:29:01 -0400

    Change `HomeRoute` to fetch game ids from server on "mount"

--- ui-rs/crates/mines_uirs/src/routes/home_route.rs
+++ ui-rs/crates/mines_uirs/src/routes/home_route.rs
@@ -1,38 +1,86 @@
+use anyhow::Error;
+use yew::format::Json;
 use yew::prelude::*;
+use yew::services::fetch::FetchTask;
 
+use crate::api;
+use crate::api::GameId;
 use crate::components::GameList;
+use crate::util::{RENDER, SKIP_RENDER};
 
 pub struct HomeRoute {
-    game_ids: Vec<String>,
+    /// Error representing a non-200 status response from the server
+    error: Option<Error>,
+    /// List of Game identifiers to render
+    game_ids: Vec<GameId>,
+    /// Pathway to send messages to self
+    link: ComponentLink<Self>,
+    /// In flight HTTP request
+    task: Option<FetchTask>,
 }
 
-enum HomeMsg {
-    CreateGame(u16, u16),
-    CreateGameSuccess(String),
-    CreateGameError(String),
+pub enum HomeRouteMsg {
+    /// Start a request for games
+    ListGames,
+    /// Received a successful response containing game ids
+    ListGamesSuccess(Vec<GameId>),
+    /// Received an error response from the request for game ids
+    ListGamesError(Error),
 }
 
 #[derive(Clone, Properties)]
 pub struct Props {
-    pub initial_game_ids: Vec<String>,
+    /// The initial list of identifiers for games
+    pub initial_game_ids: Vec<GameId>,
+}
+
+/// Create `HomeRouteMsg` from the given `FetchResponse`. The `response` is broken into parts and
+/// use to construct a list of game ids or to respond with an error.
+fn handle_list_response(response: api::FetchResponse<Vec<GameId>>) -> HomeRouteMsg {
+    let (_, Json(data)) = response.into_parts();
+    match data {
+        Ok(game_ids) => HomeRouteMsg::ListGamesSuccess(game_ids),
+        Err(err) => HomeRouteMsg::ListGamesError(err),
+    }
 }
 
 impl Component for HomeRoute {
-    type Message = ();
+    type Message = HomeRouteMsg;
     type Properties = Props;
 
-    fn create(props: Self::Properties, _link: ComponentLink<Self>) -> Self {
+    fn create(props: Self::Properties, link: ComponentLink<Self>) -> Self {
+        link.send_message(HomeRouteMsg::ListGames);
         HomeRoute {
+            error: None,
             game_ids: props.initial_game_ids,
+            link,
+            task: None,
         }
     }
 
     fn change(&mut self, _props: Self::Properties) -> ShouldRender {
-        false
+        SKIP_RENDER
     }
 
-    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
-        true
+    fn update(&mut self, message: Self::Message) -> ShouldRender {
+        match message {
+            HomeRouteMsg::ListGames => {
+                let handler = self.link.callback(handle_list_response);
+                self.task = Some(api::fetch_game_ids(handler));
+                RENDER
+            }
+            HomeRouteMsg::ListGamesSuccess(game_ids) => {
+                self.error = None;
+                self.game_ids = game_ids;
+                self.task = None;
+                RENDER
+            }
+            HomeRouteMsg::ListGamesError(error) => {
+                self.error = Some(error);
+                self.task = None;
+                RENDER
+            }
+        }
     }
 
     fn view(&self) -> Html {
