facc04bff81609850566776988d3af147e78250f
Bryan Swift <bryan@bryanjswift.com>
2020-10-12 11:17:07 -0400

    Add `Routes::Game` and let the `Root` render something for it

--- ui-rs/crates/mines_uirs/src/routes/mod.rs
+++ ui-rs/crates/mines_uirs/src/routes/mod.rs
@@ -1,9 +1,13 @@
-pub mod home_route;
+mod home_route;
 
 use yew_router::prelude::*;
 
+pub use home_route::HomeRoute;
+
 #[derive(Switch, Debug, Clone)]
 pub enum Routes {
+    #[to = "/game/{id}"]
+    Game(String),
     #[to = "/"]
     Home,
 }
