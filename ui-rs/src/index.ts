3b1397527ee97584cd9570a257eb288e119fef11
Bryan Swift <bryan@bryanjswift.com>
2021-01-07 22:02:10 -0500

    Update webpack to v5

--- ui-rs/src/index.ts
+++ ui-rs/src/index.ts
@@ -1,7 +1,7 @@
 function main(): void {
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-expect-error
-  import('./renderer').then((module) => module.buildUi());
+  import('./crate/mines_uirs').then((module) => module.buildUi());
 }
 
 main();
--- ui-rs/src/renderer.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-// This has to be a JS file because webpack's ts-loader or some other
-// compilation step removes the part of this that causes a code split and there
-// needs to be a code split or the WASM module will not be loaded.
-// See https://github.com/rustwasm/wasm-bindgen/issues/700
-export function buildUi() {
-  import('./crate/mines_uirs');
-}
