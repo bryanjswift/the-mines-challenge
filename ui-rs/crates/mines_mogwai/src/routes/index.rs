960ac91ef8f9030b4f46e1cf092ba7d7005d48d4
Bryan Swift <bryan@bryanjswift.com>
2021-01-23 19:26:54 -0500

    Add elements to select difficulty of new game

--- ui-rs/crates/mines_mogwai/src/routes/index.rs
+++ ui-rs/crates/mines_mogwai/src/routes/index.rs
@@ -1,18 +1,10 @@
-use crate::api;
+use crate::{api, Route};
 use mogwai::prelude::*;
 
-pub struct Main {}
-
-#[derive(Clone, Copy, Debug)]
-pub enum MainModel {
-    Create { rows: usize, cols: usize },
-}
-
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
-pub enum MainView {
-    Creating,
-    CreateGameSuccess(api::GameId),
-    CreateGameError(api::model::FetchError),
+/// Holds the state for showing a "Create New Game" button with the button generating a game with
+/// variable size (i.e. difficulty).
+struct Main {
+    difficulty: Difficulty,
 }
 
 impl Component for Main {
@@ -30,14 +22,17 @@ impl Component for Main {
             Ok(response) => MainView::CreateGameSuccess(response.id),
             Err(err) => MainView::CreateGameError(*err),
         });
+        use MainModel::*;
         match msg {
-            MainModel::Create { rows, cols } => {
+            Create => {
                 tx.send(&MainView::Creating);
-                api_tx.send_async(api::create_game(api::model::GameCreateInput {
-                    rows: *rows,
-                    columns: *cols,
-                }));
+                api_tx.send_async(api::create_game(self.difficulty.into()));
             }
+            SetDifficulty(difficulty) if *difficulty != self.difficulty => {
+                self.difficulty = *difficulty;
+                tx.send(&MainView::DifficultyChanged(*difficulty))
+            }
+            _ => ()
         }
     }
 
@@ -47,13 +42,149 @@ impl Component for Main {
         tx: &Transmitter<Self::ModelMsg>,
         rx: &Receiver<Self::ViewMsg>,
     ) -> ViewBuilder<Self::DomNode> {
+        let rx_difficulty = rx.branch_fold(self.difficulty, |current: &mut Difficulty, msg| match msg {
+            MainView::DifficultyChanged(difficulty) => *difficulty,
+            _ => *current,
+        });
+        let rx_size: Receiver<(usize, usize)> = rx_difficulty.branch_map(|difficulty| difficulty.into());
+        let (initial_rows, initial_cols): (usize, usize) = self.difficulty.into();
+        let rows = (format!("{}", initial_rows), rx_size.branch_map(|(rows, _)| format!("{}", rows)));
+        let cols = (format!("{}", initial_cols), rx_size.branch_map(|(_, cols)| format!("{}", cols)));
         builder! {
-            <main class="">
+            <section>
                 <h1>"Letʼs Play Minesweeper"</h1>
-                <button on:click=tx.contra_map(|_| MainModel::Create { rows: 10, cols: 10 }) boolean:disabled={rx.branch_map(|msg| msg == &MainView::Creating)}>
-                    "Start New Game"
-                </button>
-            </main>
+                <h2>"Select Difficulty"</h2>
+                <nav>
+                    <ol>
+                        {self.li_difficulty(tx, &rx_difficulty, Difficulty::Small)}
+                        {self.li_difficulty(tx, &rx_difficulty, Difficulty::Medium)}
+                        {self.li_difficulty(tx, &rx_difficulty, Difficulty::Large)}
+                    </ol>
+                </nav>
+                <form
+                    method="POST"
+                    action="/game"
+                    on:submit=tx.contra_map(|e: &Event| {
+                        e.prevent_default();
+                        MainModel::Create
+                    })
+                >
+                    <input type="hidden" name="rows" value=rows />
+                    <input type="hidden" name="cols" value=cols />
+                    <button
+                        type="submit"
+                        boolean:disabled={rx.branch_map(|msg| msg == &MainView::Creating)}
+                    >
+                        "Start New Game"
+                    </button>
+                </form>
+            </section>
+        }
+    }
+}
+
+/// The "model" messages to describe the change to effect within the `Component`.
+#[derive(Clone, Copy, Debug)]
+enum MainModel {
+    /// The `Create` message indicates a new game should be created with the currently set
+    /// `Difficulty`.
+    Create,
+    /// The `SetDifficulty` message is sent to update the currently set `Difficulty`.
+    SetDifficulty(Difficulty)
+}
+
+/// The "view" events used to trigger changes to how the `Main` `Component` is rendered.
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+enum MainView {
+    /// The `Creating` event informs the view that a request to create a new game is in flight.
+    Creating,
+    /// The `CreateGameSuccess` event indicates the request to create a new game returned
+    /// successfully, creating a game with the passed `api::GameId`.
+    CreateGameSuccess(api::GameId),
+    /// The `CreateGameError` event indicates the request to create a new game resulted in a
+    /// failure. Details about the failure are indicated by the `api::model::FetchError`.
+    CreateGameError(api::model::FetchError),
+    /// The `DifficultyChanged` event is triggered with the currently selected `Difficulty` has
+    /// been updated in the `Component`.
+    DifficultyChanged(Difficulty),
+}
+
+/// Defines preset difficulty values for the games.
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+enum Difficulty {
+    Small,
+    Medium,
+    Large,
+}
+
+/// Use the `Display` trait to describe how `Difficulty` should be turned into "human readable"
+/// text.
+impl std::fmt::Display for Difficulty {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let (rows, columns): (usize, usize) = self.into();
+        f.write_fmt(format_args!("{:?} ({} x {})", self, rows, columns))
+    }
+}
+
+/// Define the conversion from `Difficulty` to a tuple representing (rows, columns).
+impl From<Difficulty> for (usize, usize) {
+    fn from(difficulty: Difficulty) -> Self {
+        match difficulty {
+            Difficulty::Small => (5, 5),
+            Difficulty::Medium => (10, 10),
+            Difficulty::Large => (25, 25),
+        }
+    }
+}
+
+/// Define the conversion from `&Difficulty` to a tuple representing (rows, columns). Uses the
+/// `Difficulty` to tuple conversion.
+impl From<&Difficulty> for (usize, usize) {
+    fn from(difficulty: &Difficulty) -> Self {
+        let difficulty = *difficulty;
+        difficulty.into()
+    }
+}
+
+/// Define the conversion from `Difficulty` to `GameCreateInput` in terms of columns and rows.
+impl From<Difficulty> for api::model::GameCreateInput {
+    fn from(difficulty: Difficulty) -> Self {
+        let (rows, columns) = difficulty.into();
+        Self {
+            columns,
+            rows,
+        }
+    }
+}
+
+impl Main {
+    /// Display an `<li>` which handles click events to set the difficulty related to the button to
+    /// create games.
+    #[allow(unused_braces)]
+    fn li_difficulty(
+        &self,
+        tx: &Transmitter<MainModel>,
+        rx: &Receiver<Difficulty>,
+        difficulty: Difficulty
+    ) -> ViewBuilder<HtmlElement> {
+        let initial_class = if self.difficulty == difficulty {
+            String::from("active")
+        } else {
+            String::from("")
+        };
+        let rx_class = rx.branch_map(move |current_difficulty| if *current_difficulty == difficulty {
+            String::from("active")
+        } else {
+            String::from("")
+        });
+        let class_effect = (initial_class, rx_class);
+        builder! {
+            <li
+                class=class_effect
+                on:click=tx.contra_map(move |_| MainModel::SetDifficulty(difficulty))
+            >
+                {difficulty.to_string()}
+            </li>
         }
     }
 }
@@ -64,7 +195,9 @@ pub fn home() -> ViewBuilder<HtmlElement> {
     // Create a transmitter to send button clicks into.
     let tx_click = Transmitter::new();
     let rx_org = Receiver::new();
-    let main_component = Gizmo::from(Main {});
+    let main_component = Gizmo::from(Main {
+        difficulty: Difficulty::Medium,
+    });
     builder! {
         <main class="container">
             <div class="overlay">
